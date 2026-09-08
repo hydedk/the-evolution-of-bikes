fbe908b99fbf25b3dee851850b63ad3a47a5a49d
hydedk <hyde@hyde.dk>
2026-09-08 16:05:15 +0200

    Update site URL and base path in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://hydedk.github.io',
-  base: '/the-evolution-of-bikes',
+  site: 'https://teob.dk',
+  base: '',
   output: 'static',
 });

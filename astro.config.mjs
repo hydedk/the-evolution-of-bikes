8d126f0f45f90fe540377193fdb15a7f3b5d877f
hydedk <hydedk@users.noreply.github.com>
2026-09-09 17:55:36 +0200

    Translate Wim van Est story to English

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,7 @@ const languagePairs = {
   '/historier/soevnloeshed-smoer-og-kolde-oel/': '/en/stories/soevnloeshed-smoer-og-kolde-oel/',
   '/historier/aldo-bini-maglia-nera-1948/': '/en/stories/aldo-bini-maglia-nera-1948/',
   '/historier/da-michelin-navnet-blev-et-problem/': '/en/stories/da-michelin-navnet-blev-et-problem/',
+  '/historier/wim-van-est-aubisque-1951/': '/en/stories/wim-van-est-aubisque-1951/',
 };
 const pairedPages = Object.fromEntries(Object.entries(languagePairs).flatMap(([da, en]) => [[da, { da, en }], [en, { da, en }]]));
 const seoTitleOverrides = {

2ce0041b0bc26bacbb960dabf8d53e4ad1db0016
hydedk <hydedk@users.noreply.github.com>
2026-09-09 17:28:51 +0200

    Translate Alfonsina Strada story to English

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ const languagePairs = {
   '/perioder/': '/en/periods/', '/cykler/': '/en/bikes/', '/komponenter/': '/en/components/',
   '/historier/cykelsportens-vilde-vesten/': '/en/stories/cykelsportens-vilde-vesten/',
   '/historier/snyd-soem-tour-de-france-1904/': '/en/stories/snyd-soem-tour-de-france-1904/',
+  '/historier/alfonsina-strada-giro-1924/': '/en/stories/alfonsina-strada-giro-1924/',
 };
 const pairedPages = Object.fromEntries(Object.entries(languagePairs).flatMap(([da, en]) => [[da, { da, en }], [en, { da, en }]]));
 const seoTitleOverrides = {

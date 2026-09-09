ebdf7275da8ea837f9b352c68646f01b9284cfda
hydedk <hydedk@users.noreply.github.com>
2026-09-09 18:51:55 +0200

    Translate SunTour and Asmussen stories

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,6 +23,8 @@ const languagePairs = {
   '/historier/anquetil-poulidor-puy-de-dome-1964/': '/en/stories/anquetil-poulidor-puy-de-dome-1964/',
   '/historier/tour-feltets-dopingprotest-1966/': '/en/stories/tour-feltets-dopingprotest-1966/',
   '/historier/tom-simpson-mont-ventoux-1967/': '/en/stories/tom-simpson-mont-ventoux-1967/',
+  '/historier/suntour-shimano-gearkrigen/': '/en/stories/suntour-shimano-gearkrigen/',
+  '/historier/gunnar-asmussen-funny-bike/': '/en/stories/gunnar-asmussen-funny-bike/',
 };
 const pairedPages = Object.fromEntries(Object.entries(languagePairs).flatMap(([da, en]) => [[da, { da, en }], [en, { da, en }]]));
 const seoTitleOverrides = {
